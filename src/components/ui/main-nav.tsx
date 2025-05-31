821f4da5619269c2d57d9d77c5799b072294d8a7
Deepak Barwal <deepakbarwal98@gmail.com>
2025-05-31 17:31:04 +0530

    add: shadcn nav menu

---
+++ src/components/ui/main-nav.tsx
@@ -0,0 +1,108 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
+import { cn } from "@/lib/utils";
+
+const posts: { title: string; href: string; description: string }[] = [
+  {
+    title: "React",
+    href: "/blog/react",
+    description: "Learn react and next",
+  },
+  {
+    title: "React",
+    href: "/blog/react",
+    description: "Learn react and next",
+  },
+  {
+    title: "React",
+    href: "/blog/react",
+    description: "Learn react and next",
+  },
+  {
+    title: "React",
+    href: "/blog/react",
+    description: "Learn react and next",
+  },
+  {
+    title: "React",
+    href: "/blog/react",
+    description: "Learn react and next",
+  },
+];
+
+export default function MainNav() {
+  return (
+    <div
+      className={cn(
+        "flex flex-col items-strt justify-start md:flex-row md:items-center md:justify-between pt-10 z-50"
+      )}
+    >
+      <Link href={"/"}>
+        <div>
+          {/* Icon */}
+          <p>Deepak Barwal&apos;s Blog</p>
+        </div>
+      </Link>
+      <NavigationMenu viewport={false}>
+        <NavigationMenuList>
+          <NavigationMenuItem>
+            <NavigationMenuTrigger>Posts</NavigationMenuTrigger>
+            <NavigationMenuContent>
+              <ul className="grid w-[400px] gap-2 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
+                {posts.map((post) => (
+                  <ListItem
+                    key={post.title}
+                    title={post.title}
+                    href={post.href}
+                  >
+                    {post.description}
+                  </ListItem>
+                ))}
+              </ul>
+            </NavigationMenuContent>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              asChild
+              className={navigationMenuTriggerStyle()}
+            >
+              <Link href="/about">About</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+        </NavigationMenuList>
+      </NavigationMenu>
+    </div>
+  );
+}
+
+function ListItem({
+  title,
+  children,
+  href,
+  ...props
+}: React.ComponentPropsWithoutRef<"li"> & { href: string }) {
+  return (
+    <li {...props}>
+      <NavigationMenuLink asChild>
+        <Link href={href}>
+          <div className="text-sm leading-none font-medium">{title}</div>
+          <p className="text-muted-foreground line-clamp-2 text-sm leading-snug">
+            {children}
+          </p>
+        </Link>
+      </NavigationMenuLink>
+    </li>
+  );
+}
