f648f490b3bea49aa0e464c1bdb43388de4abcf2
Deepak Barwal <deepakbarwal98@gmail.com>
2025-05-31 17:11:52 +0530

    add: mdx files and utils to read them

---
+++ src/app/blog/utils.ts
@@ -0,0 +1,71 @@
+import fs from "node:fs";
+import path from "node:path";
+import matter from "gray-matter";
+
+// get all the mdx files from the dir
+function getMdxFiles(dir: string) {
+  return fs.readdirSync(dir).filter((file) => path.extname(file) === ".mdx");
+}
+
+// read data from those files
+function readMdxFile(filePath: fs.PathOrFileDescriptor) {
+  const rawContent = fs.readFileSync(filePath, "utf-8");
+  return matter(rawContent);
+}
+
+// present the mdx data and metadata
+function getMdxData(dir: string) {
+  const mdxFiles = getMdxFiles(dir);
+
+  return mdxFiles.map((file) => {
+    const { data: metadata, content } = readMdxFile(path.join(dir, file));
+    const slug = path.basename(file, path.extname(file));
+
+    return {
+      metadata,
+      slug,
+      content,
+    };
+  });
+}
+
+export function getBlogPosts() {
+  return getMdxData(path.join(process.cwd(), "src", "app", "blog", "contents"));
+}
+
+export function formatDate(date: string, includeRelative = false) {
+  const currentDate = new Date();
+  if (!date.includes("T")) {
+    date = `${date}T00:00:00`;
+  }
+
+  const targetDate = new Date(date);
+
+  const yearsAgo = currentDate.getFullYear() - targetDate.getFullYear();
+  const monthsAgo = currentDate.getMonth() - targetDate.getMonth();
+  const daysAgo = currentDate.getDate() - targetDate.getDate();
+
+  let formattedDate = "";
+
+  if (yearsAgo > 0) {
+    formattedDate = `${yearsAgo}y ago`;
+  } else if (monthsAgo > 0) {
+    formattedDate = `${monthsAgo}mo ago`;
+  } else if (daysAgo > 0) {
+    formattedDate = `${daysAgo}d ago`;
+  } else {
+    formattedDate = "Today";
+  }
+
+  const fullDate = targetDate.toLocaleString("en-US", {
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
+
+  if (!includeRelative) {
+    return fullDate;
+  }
+
+  return `${fullDate} (${formattedDate})`;
+}
